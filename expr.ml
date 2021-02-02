f8f4bb380e6fddc4fb26541e1e1753c0ebc14913
Elodie <542-eberna08@users.noreply.gitlab.aliens-lyon.fr>
2021-02-02 12:08:57 +0100

    Ajout de la possibilité de mettre l'option paf

---
+++ expr.ml
@@ -0,0 +1,1 @@
+let paf = ref false
--- main.ml
+++ main.ml
@@ -1,33 +1,116 @@
 open Cell
 open Sheet
 open Command
+open Expr
 
 (*** début de la partie "incantatoire" ***)
 (* stdin désigne l'entrée standard (le clavier) *)
 (* lexbuf est un canal ouvert sur stdin *)
 let lexbuf = Lexing.from_channel stdin
-
-(* on enchaîne les tuyaux: lexbuf est passé à Lexer.token,
-   et le résultat est donné à Parser.main *)
-
-let parse () = Parser.debut Lexer.token lexbuf
 (*** fin de la partie "incantatoire" ***)
 
+let compile e =
+  begin
+    print_newline();
+    run_script e;
+    if !paf then print_int 42;
+    print_newline()
+  end
+
+
+let nom_fichier = ref ""
+
 let spreadsheet () =
-      let result = parse () in
+      (* on decrit les diverses options possibles ; Arg.Set dit de mettre
+     un bool ref a vrai si on active l'option.  
+     Voir la librairie "Arg" de Caml pour davantage de details *)
+    let optlist = [
+        ("-paf", Arg.Set paf, "Active le mode PAF, si on trouve une boucle le programme s'arrête." );
+    ] in
+
+    let usage = "Bienvenue a bord." in  (* message d'accueil *)
+    Arg.parse (* ci-dessous les 3 arguments de Arg.parse : *)
+    optlist (* la liste des options definie ci-dessus *)
+
+    (fun s -> ()) (* la fonction a declencher lorsqu'on recupere un string qui n'est pas une option : ici c'est le nom du fichier, et on stocke cette information dans la reference nom_fichier *)
+    usage; (* le message d'accueil *)
+
+    try
+    (* les incantations d'usage pour aller lire dans le fichier et
+       faire les analyses lexicale et syntaxique *)
+
+    let parse () = Parser.debut Lexer.token lexbuf in
+    let result = parse () in
+    (* on a recupere une expressions dans result, on peut lancer la
+       fonction compile, definie plus haut *)
+    compile result; flush stdout
+    with _ -> (print_string "erreur de saisie\n")
+
+
+      (*let result = parse () in
       begin
         (* le seul endroit a comprendre (dans un premier temps) :
            appel a la fonction run_script, qui est definie dans command.ml *)
         run_script result;
         flush stdout;
-      end
+      end*)
 ;;
 
 
 let _ = spreadsheet()
 
-(* let _ = let scr = [ Upd (("C",2), Cst 3.7); ShowAll ] in
- *         begin
- *           print_string "HAHAHA!!!\n";
- *           run_script scr
- *         end *)
+(*
+open Expr
+
+let compile e =
+  begin
+    affiche_expr e;
+    print_newline();
+    print_int (eval e);
+    print_newline()
+  end
+
+(* stdin désigne l'entrée standard (le clavier) *)
+(* lexbuf est un canal ouvert sur stdin *)
+
+let lexbuf = Lexing.from_channel stdin
+
+(* on enchaîne les tuyaux: lexbuf est passé à Lexer.token,
+   et le résultat est donné à Parser.main *)
+
+let nom_fichier = ref ""
+  
+let fonction_principale () =
+  (* on decrit les diverses options possibles ; Arg.Set dit de mettre
+     un bool ref a vrai si on active l'option.  
+     Voir la librairie "Arg" de Caml pour davantage de details *)
+  let optlist = [
+    ("-debug", Arg.Set verbose, "Active le mode de debuggage" );
+    ("-shout", Arg.Set lettres_capitales, "Ecrit les operateurs en majuscules")
+  ] in
+  let usage = "Bienvenue a bord." in  (* message d'accueil *)
+  Arg.parse (* ci-dessous les 3 arguments de Arg.parse : *)
+    optlist (* la liste des options definie ci-dessus *)
+
+    (fun s -> nom_fichier := s) (* la fonction a declencher lorsqu'on recupere un string qui n'est pas une option : ici c'est le nom du fichier, et on stocke cette information dans la reference nom_fichier *)
+    usage; (* le message d'accueil *)
+  try
+    (* les incantations d'usage pour aller lire dans le fichier et
+       faire les analyses lexicale et syntaxique *)
+    let in_file = open_in !nom_fichier in
+    let lexbuf_file = Lexing.from_channel in_file in
+    let parse () = Parser.main Lexer.token lexbuf_file in
+    let result = parse () in
+    (* on a recupere une expressions dans result, on peut lancer la
+       fonction compile, definie plus haut *)
+    compile result; flush stdout
+  with _ -> (print_string "erreur de saisie\n")
+
+
+let _ = fonction_principale()
+
+
+
+
+
+*)
